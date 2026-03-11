acdee7d86915ebf43a1147818c6284f7ff100f13
K-Neri <kwizeraneri@gmail.com>
2026-03-11 20:30:10 +0100

    incrementation soleil, eau et pause

--- tracker.js
+++ tracker.js
@@ -1,13 +1,24 @@
-let btn1 = document.getElementById('btn_count1');
-let cnt = document.getElementById('val_count');
-let btn5= document.getElementById('btn_count5');
+let btn_sun = document.getElementById('btn-sun');
+let cnt_sun = document.getElementById('sun-value');
+
+let btn_water= document.getElementById('btn-water');
+let cnt_water = document.getElementById('water-value');
 
 
-btn1.addEventListener('click', () => {
-    cnt.innerText = parseInt(cnt.innerText) + 1;
+let btn_break= document.getElementById('btn-break');
+let cnt_break = document.getElementById('break-value');
+
+btn_sun.addEventListener('click', () => {
+    cnt_sun.innerText = parseInt(cnt_sun.innerText) + 1;
 });
 
- 
+btn_water.addEventListener('click', () => {
+    cnt_water.innerText = parseInt(cnt_water.innerText) + 1;
+});
+
+btn_break.addEventListener('click', () => {
+    cnt_break.innerText = parseInt(cnt_break.innerText) + 1;
+});
 
 
 btn5.addEventListener('click', () => {
