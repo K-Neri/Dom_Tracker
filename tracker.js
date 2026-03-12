89ed5354ef37a90cdc7d9cc335ec91a923efdef1
K-Neri <kwizeraneri@gmail.com>
2026-03-12 20:42:16 +0100

    premier compteur

--- tracker.js
+++ tracker.js
@@ -11,31 +11,58 @@ let total_value = document.getElementById('total-value');
 
 let btn_reset = document.getElementById('btn-reset');
 
+let remaining_sun = document.getElementById('remaining_sun');
+let remaining_water = document.getElementById('remaining_water');
+let remaining_break = document.getElementById('remaining_break');
 
 
-function reset(){
+
+function reset() {
     cnt_sun.innerText = 0;
     cnt_water.innerText = 0;
     cnt_break.innerText = 0;
     total_value.innerText = 0;
+
+    remaining_sun.innerText = 10;
+    remaining_water.innerText = 10;
+    remaining_break.innerText = 10;
 }
-function total(){
+
+function total() {
+
     total_value.innerText = parseInt(total_value.innerText) + 1;
 }
-    
+
+
+
 btn_sun.addEventListener('click', () => {
     total();
-    cnt_sun.innerText = parseInt(cnt_sun.innerText) + 1;
+
+    if (remaining_sun > 0 && remaining_sun <= 10) {
+
+        cnt_sun.innerText = parseInt(cnt_sun.innerText) + 1;
+        remaining_sun= remaining_sun - 1;
+    }
+
+
 });
 
 btn_water.addEventListener('click', () => {
     total();
-    cnt_water.innerText = parseInt(cnt_water.innerText) + 1;
+
+    if (remaining_water.innerText > 0 && remaining_water.innerText <= 10) {
+        cnt_water.innerText = parseInt(cnt_water.innerText) + 1;
+        remaining_water.innerText = parseInt(remaining_water.innerText) - 1;
+    }
+
 });
 
 btn_break.addEventListener('click', () => {
     total();
-    cnt_break.innerText = parseInt(cnt_break.innerText) + 1;
+    if (remaining_break.innerText > 0 && remaining_break.innerText <= 10) {
+        cnt_break.innerText = parseInt(cnt_break.innerText) + 1;
+        remaining_break.innerText = parseInt(remaining_break.innerText) - 1;
+    }
 });
 
 btn_reset.addEventListener('click', reset);
